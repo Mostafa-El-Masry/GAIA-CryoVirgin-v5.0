9437580f4e42f47c3b7c5f09ac4d2f66de5c61bc
Mostafa-El-Masry <Mostafa.abdelfattah2021@gmail.com>
2025-11-28 19:07:26 +0300

    fixing academy

--- app/apollo/layout.tsx
+++ app/apollo/layout.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
-import PermissionGate from "@/components/permissions/PermissionGate";
-import LessonGate from "@/components/permissions/LessonGate";
 
 export default function ApolloLayout({ children }: { children: ReactNode }) {
-  return (
-    <PermissionGate permission="apollo">
-      <LessonGate featureLabel="Apollo">{children}</LessonGate>
-    </PermissionGate>
-  );
+  return <>{children}</>;
 }
